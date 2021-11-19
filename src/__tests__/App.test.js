c90e596c766fa20e7262c17c44d12dfdb5529716
Nita <rahmanitaazwan@gmail.com>
2021-11-19 21:28:00 +0700

    selesai

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,70 @@
+
+import { AddTodo } from '../components/helper';
+
+test('Should add todo to the list', () => {
+  // Arrange
+const defaultTodo = [{
+  id: 1,
+  name: "Learn JSX",
+  isComplete: false,
+},
+{
+  id: 2,
+  name: "Build awesome react app",
+  isComplete: false,
+},
+{
+  id: 3,
+  name: "Ship it",
+  isComplete: false,
+},
+];
+
+const addTodo ={
+  id: 3,
+  name: "newTodo",
+  isComplete: false,
+};
+const concat  = [addTodo, ...defaultTodo]
+const expected = JSON.stringify(concat)
+  // Act
+const newTodo = AddTodo(defaultTodo, addTodo);
+const result = JSON.stringify(newTodo)
+  // Assert
+expect(result).toBe(expected);
+});
+
+test('should not mutate the existing todo array', () => {
+  // Arrange
+const defaultTodo = [{
+  id: 1,
+  name: "Learn JSX",
+  isComplete: false,
+},
+{
+  id: 2,
+  nanme: "Build awesome react app"
+  isComplete: false,
+},
+{
+
+  id: 3,
+  name: "Ship it",
+  isComplete: false,
+},
+];
+
+const addTodo = {
+  id: 3,
+  name: "newTodo",
+  isComplete: false,
+};
+const notExpected = JSON.stringify(addTodo)
+  
+  // Act
+sortAndDeduplicateDiagnostics(todoList, newValue);
+const newTodo = AddTodo(defaultTodo, addTodo);
+const result = JSON.stringify(newTodo) 
+  // Assert
+expect(result)not.toBe(notExpected);
+});
